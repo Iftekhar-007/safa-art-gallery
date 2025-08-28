bf5744b1ff3b807c17868bb099d2369f1d9bdcc9
Iftekhar-007 <shawonsp534@gmail.com>
2025-08-28 16:27:01 +0600

    2nd commit

---
+++ src/lib/dbconnect.js
@@ -0,0 +1,26 @@
+// lib/dbConnect.js
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+
+let client;
+let clientPromise;
+
+if (!clientPromise) {
+  client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  clientPromise = client.connect();
+}
+
+export default async function dbConnect(collectionName) {
+  const connectedClient = await clientPromise;
+  return connectedClient
+    .db(process.env.DB_USER_NAME)
+    .collection(collectionName);
+}
