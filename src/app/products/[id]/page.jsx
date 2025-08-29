627c80c80780760233ad7d3ba46bc68997c6e60c
Iftekhar-007 <shawonsp534@gmail.com>
2025-08-29 18:05:42 +0600

    okay

---
+++ src/app/products/[id]/page.jsx
@@ -0,0 +1,24 @@
+import dbConnect from "@/lib/dbconnect";
+import { ObjectId } from "mongodb";
+import Image from "next/image";
+import React from "react";
+
+const page = async ({ params }) => {
+  const { id } = params;
+
+  const productsCollection = await dbConnect("products");
+  const product = await productsCollection.findOne({ _id: new ObjectId(id) });
+
+  return (
+    <div className="py-20">
+      <img src={product.image} alt={product.title} />
+      <h2>details {product.title}</h2>
+      <p className="text-lg text-gray-700 mb-2">{product.description}</p>
+      <p className="text-sm text-gray-500">Sign: {product.sign}</p>
+      <p className="text-sm text-gray-500">Date: {product.date}</p>
+      <p className="text-sm text-gray-500">Time: {product.time}</p>
+    </div>
+  );
+};
+
+export default page;
