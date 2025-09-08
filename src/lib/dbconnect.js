673c6858e19c49462ff57a197d1a9f257498ed88
Iftekhar-007 <shawonsp534@gmail.com>
2025-09-08 23:28:09 +0600

    okay

--- src/lib/dbconnect.js
+++ src/lib/dbconnect.js
@@ -1,4 +1,30 @@
-// lib/dbConnect.js
+// // lib/dbConnect.js
+// import { MongoClient, ServerApiVersion } from "mongodb";
+
+// const uri = process.env.MONGODB_URI;
+
+// let client;
+// let clientPromise;
+
+// if (!clientPromise) {
+//   client = new MongoClient(uri, {
+//     serverApi: {
+//       version: ServerApiVersion.v1,
+//       strict: true,
+//       deprecationErrors: true,
+//     },
+//   });
+
+//   clientPromise = client.connect();
+// }
+
+// export default async function dbConnect(collectionName) {
+//   const connectedClient = await clientPromise;
+//   return connectedClient
+//     .db(process.env.DB_USER_NAME)
+//     .collection(collectionName);
+// }
+
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
@@ -18,9 +44,11 @@ if (!clientPromise) {
   clientPromise = client.connect();
 }
 
-export default async function dbConnect(collectionName) {
-  const connectedClient = await clientPromise;
-  return connectedClient
-    .db(process.env.DB_USER_NAME)
-    .collection(collectionName);
-}
+const dbConnect = async (collectionName) => {
+  const client = await clientPromise;
+
+  const db = client.db(process.env.DB_USER_NAME);
+  return db.collection(collectionName);
+};
+
+export default dbConnect;
