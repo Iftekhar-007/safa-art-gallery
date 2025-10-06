1060b3c6cae03ea45e3eed78f8e503e80e38675d
Iftekhar-007 <shawonsp534@gmail.com>
2025-10-06 19:13:14 +0600

    okay

--- src/app/dashboard/wishlist/page.jsx
+++ src/app/dashboard/wishlist/page.jsx
@@ -31,33 +31,6 @@ const MyWishlist = () => {
   const { data: session } = useSession();
   const [wishlist, setWishlist] = useState([]);
 
-  //   useEffect(() => {
-  //     fetch("/api/wishlist")
-  //       .then((res) => res.json())
-  //       .then((data) => setWishlist(data));
-  //   }, []);
-
-  //   return (
-  //     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-  //       <h1 className="text-3xl md:text-4xl font-bold text-center mb-10 text-gray-100">
-  //         My Wishlist ❤️
-  //       </h1>
-
-  //       {wishlist.length === 0 ? (
-  //         <p className="text-center text-gray-500 text-lg">
-  //           Your wishlist is empty.
-  //         </p>
-  //       ) : (
-  //         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-  //           {wishlist.map((pro) => (
-  //             <WishCard key={pro.id} pro={pro} />
-  //           ))}
-  //         </div>
-  //       )}
-  //     </div>
-  //   );
-  // };
-
   useEffect(() => {
     if (session?.user?.email) {
       fetch(`/api/wishlist?email=${session.user.email}`)
